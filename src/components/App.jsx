145a028274ebaf56fad70eaaa8664f827324bfc1
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-03 17:21:50 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import { Component } from 'react';
+import FormNewContact from './FormNewContact/FormNewContact';
+import Section from './Section/Section';
+
+export class App extends Component {
+  state = { contacts: [], name: '', number: '' };
+
+  render() {
+    return (
+      <>
+        <Section title={'Phonebook'}>
+          <FormNewContact name={this.state.name} />
+        </Section>
+        <Section title={'Contacts'}></Section>
+      </>
+    );
+  }
+}
