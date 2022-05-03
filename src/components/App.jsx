7738e5d9b6644c4fa6222de2734c741a104fb51a
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-03 17:39:33 +0300

    form added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,27 @@ import Section from './Section/Section';
 export class App extends Component {
   state = { contacts: [], name: '', number: '' };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  handleChange = e => {
+    console.log(e);
+
+    this.setState({ name: e.target.value });
+  };
+
   render() {
+    const { name, number } = this.state;
     return (
       <>
         <Section title={'Phonebook'}>
-          <FormNewContact name={this.state.name} />
+          <FormNewContact
+            name={name}
+            handleSubmit={this.handleSubmit}
+            handleChange={this.handleChange}
+          />
         </Section>
         <Section title={'Contacts'}></Section>
       </>
--- src/components/FormNewContact/FormNewContact.jsx
+++ src/components/FormNewContact/FormNewContact.jsx
@@ -1,18 +1,19 @@
-const FormNewContact = () => {
+const FormNewContact = ({ name, handleSubmit, handleChange }) => {
   return (
-    <form>
+    <form onSubmit={e => handleSubmit(e)}>
       <label>
         Input name
         <input
           type="text"
           name="name"
-          value="value"
+          value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          onChange={e => handleChange(e)}
         />
       </label>
-      <button type="button">Add contact</button>
+      <button type="submit">Add contact</button>
     </form>
   );
 };
